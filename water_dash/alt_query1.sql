6e58c91158ee6cb4d16938bc3679a5d1dc659270
MichaelCurrin <michael.currin@gmail.com>
2018-02-25 20:48:28 +0200

    feature: Create extra queries

---
+++ water_dash/alt_query1.sql
@@ -0,0 +1,12 @@
+-- worldwide tweets
+SELECT
+    trend.topic,
+    DATE(MAX(trend.timestamp)) AS last_trended,
+    DATE(MIN(trend.timestamp)) AS first_trended,
+    COUNT(DISTINCT(DATE(trend.timestamp))) AS days_mentioned,
+    MAX(trend.volume) AS highest_volume
+FROM trend
+INNER JOIN place ON trend.place_id = place.id
+GROUP BY trend.topic
+HAVING highest_volume IS NOT NULL
+ORDER BY highest_volume DESC;
