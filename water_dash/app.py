d696b4fbbe4f1f2ae13695e8fb96f68a167fbcfb
MichaelCurrin <michael.currin@gmail.com>
2018-09-14 22:15:16 +0200

    refactor: Update app.py

--- water_dash/app.py
+++ water_dash/app.py
@@ -2,19 +2,10 @@
 """
 Water dashboard main application file.
 """
-import os
-
 from flask import Flask
-from sqlalchemy import create_engine
 
 import config
 import lib
-
-assert os.access(config.db_path, os.R_OK), (
-    "Create the database or symlink then restart the application."
-    " Expected path: {}".format(config.db_path)
-)
-SQL_ENGINE = create_engine("sqlite:///{}".format(config.db_path))
 
 with open(config.query_path) as f_in:
     SQL_QUERY = f_in.read()
@@ -29,10 +20,7 @@ def root():
 
     Execute a fixed query and return the results in an HTML table form.
     """
-    conn = SQL_ENGINE.connect()
-
-    query = conn.execute(SQL_QUERY)
-    result = query.cursor.fetchall()
+    results = lib.fetch_data(SQL_QUERY)
 
     cast_result = (
         (
@@ -42,13 +30,13 @@ def root():
             row[3],
             "not available" if row[4] is None else "{:,d}".format(int(row[4]))
         )
-        for row in result
+        for row in results
     )
 
     html = lib.build_html(
         title="CPT Water Trends",
         subtitle="Water crisis topics trending in Cape Town",
-        paragraph=' Data has been scraped from the Twitter API since August'
+        paragraph='Data has been scraped from the Twitter API since August'
             ' 2017 and new data is added daily using my'
             ' <a href="https://github.com/MichaelCurrin/twitterverse">'
             'twitterverse</a> repo. '
@@ -56,7 +44,7 @@ def root():
             'Trending topics have been filtered to terms related to the water'
             ' crisis such as dam, drought, water, crisis and day zero. '
             ' The code for this Flask web server is available in my'
-            ' <a href="https://github.com/MichaelCurrin/water_crisis_dash">'
+            ' <a href="https://github.com/MichaelCurrin/water-crisis-dash">'
             'Water Crisis</a> repo on Github.'
             '<br><br> '
             'Volume is the <i>global</i> count of tweets about the topic, in'
