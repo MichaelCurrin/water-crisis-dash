c05c3105e07979494aeb62f7b7d1eac1e2826673
Michael Currin <michael.currin@gmail.com>
2019-05-20 22:54:47 +0200

    refactor: Move config values to config file

--- water_dash/app.py
+++ water_dash/app.py
@@ -24,49 +24,6 @@ with open(config.source_data_query) as f_in:
 app = Flask(__name__, static_url_path='/static')
 
 
-WATER = dict(
-    title="CPT Water Trends",
-    subtitle="Water crisis topics trending in Cape Town",
-    paragraph=' Data has been scraped from the Twitter API since August'
-        ' 2017 and new data is added daily using my'
-        ' <a href="https://github.com/MichaelCurrin/twitterverse">'
-        'twitterverse</a> repo. '
-        '<br><br> '
-        'Trending topics have been filtered to terms related to the water'
-        ' crisis such as dam, drought, water, crisis and day zero. '
-        ' The code for this Flask web server is available in my'
-        ' <a href="https://github.com/MichaelCurrin/water_crisis_dash">'
-        'Water Crisis</a> repo on Github.'
-        '<br><br> '
-        'Volume is the <i>global</i> count of tweets about the topic, in'
-        ' the past 24 hours, at the time the value was stored. The max'
-        ' volume shown below is the highest recorded value since the start'
-        ' of the available data. If not available then it was below'
-        ' 10,000 tweets and therefore was too low for Twitter to make'
-        ' available.',
-)
-ELECTION = dict(
-    title="Twitter Trends",
-    subtitle="South Africa elections 2019",
-    paragraph='''
-        The data includes trending terms on Twitter from 1 Jan 2019
-        to present. The table below has been filtered to topics which
-        trended in South Africa which match the elections, voting,
-        specific parties or similar terms.<br><br>
-    
-        See the <a href="https://github.com/MichaelCurrin/twitterverse">Twitterverse repo</a>
-        on Github.com - it was created to scrape tweets and trending topics.<br><br>
-        
-        Volume is the <i>global</i> count of tweets about the topic, in
-        the past 24 hours, at the time the value was stored. The max
-        volume shown below is the highest recorded value since the start
-        of the available data. If not available then it was below
-        10,000 tweets and therefore was too low for Twitter to make
-        available.
-    ''',
-)
-
-
 @app.route('/')
 def root():
     """Web app root.
@@ -88,7 +45,7 @@ def root():
         for row in result
     )
 
-    chosen_topic = ELECTION
+    chosen_topic = config.ELECTION
     html = lib.build_html(
         row_data=cast_result,
         **chosen_topic,
