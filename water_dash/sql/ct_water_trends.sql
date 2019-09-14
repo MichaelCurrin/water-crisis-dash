a43226e884c1103db070776944b3987152791d22
Michael Currin <michael.currin@gmail.com>
2019-09-14 17:20:13 +0200

    feat: Add exclusions to water search

--- water_dash/sql/ct_water_trends.sql
+++ water_dash/sql/ct_water_trends.sql
@@ -13,6 +13,7 @@ WHERE place.name = 'Cape Town'
        (
                trend.topic LIKE '%water%'
            AND trend.topic NOT LIKE '%watermelon%'
+           AND trend.topic NOT LIKE '%SibanyeStillwater%'
        )
        OR trend.topic LIKE '%drought%'
        OR (
@@ -24,18 +25,31 @@ WHERE place.name = 'Cape Town'
            AND trend.topic NOT LIKE '%madam%'
            AND trend.topic NOT LIKE '%damanifesto%'
            AND trend.topic NOT LIKE '%notre%dam%'
+           AND trend.topic NOT LIKE '%adams%'
+           AND trend.topic NOT LIKE '%Amsterdam%'
+           AND trend.topic NOT LIKE '%lindamti%'
           )
        OR trend.topic LIKE '%level%'
        OR trend.topic LIKE '%shortage%'
        OR trend.topic LIKE '%flush%'
        OR trend.topic LIKE '%drink%'
        OR trend.topic LIKE '%waste%'
-       OR trend.topic LIKE '%save%'
+       OR (
+           trend.topic LIKE '%save%'
+           AND trend.topic NOT LIKE '%SAvENG%'
+           AND trend.topic NOT LIKE '%davesaves%'
+           AND trend.topic NOT LIKE '%spec%savers%'
+       )
        OR trend.topic LIKE '%crisis%'
        OR trend.topic LIKE '%disaster%'
        OR trend.topic LIKE '%day%zero%'
        OR trend.topic LIKE '%shower%'
-       OR trend.topic LIKE '%bath%'
+       OR (
+           trend.topic LIKE '%bath%'
+           AND trend.topic NOT LIKE '%batho%' -- names
+           AND trend.topic NOT LIKE '%batha%'
+           AND trend.topic NOT LIKE '%bathu%'
+       )
     )
 GROUP BY trend.topic
 ORDER BY last_trended DESC;
