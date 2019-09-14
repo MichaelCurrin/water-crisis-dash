28ada84171a4a8def0695f74d9e8d5d0d3d95757
Michael Currin <michael.currin@gmail.com>
2019-09-14 16:54:25 +0200

    feat: Create cape_town_tweets.sql

---
+++ water_dash/sql/cape_town_tweets.sql
@@ -0,0 +1,14 @@
+-- Cape Town tweets in this month or the previous month.
+SELECT
+    trend.topic,
+    DATE(MAX(trend.timestamp)) AS last_trended,
+    DATE(MIN(trend.timestamp)) AS first_trended,
+    COUNT(DISTINCT(DATE(trend.timestamp))) AS days_mentioned,
+    MAX(trend.volume) AS highest_volume
+FROM trend
+INNER JOIN place ON trend.place_id = place.id
+WHERE place.name = 'Cape Town'
+GROUP BY trend.topic
+HAVING last_trended >=  DATE('now', 'start of month', '-1 month')
+   AND highest_volume IS NOT NULL
+ORDER BY last_trended DESC;
