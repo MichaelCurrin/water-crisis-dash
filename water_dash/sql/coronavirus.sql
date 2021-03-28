0ffd963165cce77fb3022466ad5bdf9c52284529
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-28 13:56:08 +0200

    Update coronavirus.sql

--- water_dash/sql/coronavirus.sql
+++ water_dash/sql/coronavirus.sql
@@ -11,13 +11,14 @@ WHERE place.name = 'Worldwide'
  AND (
        -- See https://en.wikipedia.org/wiki/Severe_acute_respiratory_syndrome_coronavirus_2
        
-       -- Take into account misspellings of corona
-          trend.topic LIKE '%c_r_n_%' 
+          trend.topic LIKE '%corona%'
+     
        -- Covid-19 and related
        OR trend.topic LIKE '%covid%'
        
        -- 2019-nCoV (from novel coronavirus).
        OR trend.topic LIKE '%nCoV%'
+     
        -- CoV-2
        OR trend.topic LIKE '%CoV%2%'
        OR trend.topic LIKE '%SARS%'
@@ -33,15 +34,12 @@ WHERE place.name = 'Worldwide'
        OR trend.topic LIKE '%lockdown%'
        
        OR trend.topic LIKE '%strain%'
+       OR trend.topic LIKE '%infect%'
        OR trend.topic LIKE '%virus%'
        OR trend.topic LIKE '%outbreak%'
        OR trend.topic LIKE '%epidemic%'
        OR trend.topic LIKE '%pandemic%'
        OR trend.topic LIKE '%disease%'
-       
-       -- Origin of the outbreak
-       OR trend.topic LIKE '%China%'
-       OR trend.topic LIKE '%Wuhan%'
      )
 GROUP BY trend.topic
 ORDER BY last_trended DESC;
