9dcc0ddc319958bfee6184e464393671906fdbb3
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-28 13:57:04 +0200

    Update coronavirus_source.sql

--- water_dash/sql/coronavirus_source.sql
+++ water_dash/sql/coronavirus_source.sql
@@ -1,4 +1,4 @@
--- Corona virus trends
+-- Corona virus trends source data for CSV.
 SELECT
     place.name AS place_name,
     DATE(trend.timestamp) AS date,
@@ -10,13 +10,14 @@ INNER JOIN place ON trend.place_id = place.id
 WHERE
        -- See https://en.wikipedia.org/wiki/Severe_acute_respiratory_syndrome_coronavirus_2
        
-       -- Take into account misspellings of corona
-          trend.topic LIKE '%c_r_n_%' 
+          trend.topic LIKE '%corona%'
+     
        -- Covid-19 and related
        OR trend.topic LIKE '%covid%'
        
        -- 2019-nCoV (from novel coronavirus).
        OR trend.topic LIKE '%nCoV%'
+     
        -- CoV-2
        OR trend.topic LIKE '%CoV%2%'
        OR trend.topic LIKE '%SARS%'
@@ -32,16 +33,12 @@ WHERE
        OR trend.topic LIKE '%lockdown%'
        
        OR trend.topic LIKE '%strain%'
+       OR trend.topic LIKE '%infect%'
        OR trend.topic LIKE '%virus%'
        OR trend.topic LIKE '%outbreak%'
        OR trend.topic LIKE '%epidemic%'
        OR trend.topic LIKE '%pandemic%'
        OR trend.topic LIKE '%disease%'
-       
-       -- Origin of the outbreak
-       OR trend.topic LIKE '%China%'
-       OR trend.topic LIKE '%Wuhan%'
 ORDER BY
     place_name,
-    trend_topic
-;
+    trend_topic;
